dddd8f560a4e05210433280daf04f755a312858f
DeaDDFuze <brasseta@gmail.com>
2022-02-23 19:34:32 +1100

    MAJ 23/02

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let article = document.getElementById("promo");
 let promo = document.getElementById("promoCode");
+let articleSet = document.getElementById("articleSet")
 
 article.addEventListener( 'mouseover', function(e){
 promo.style.opacity = "1"
@@ -17,3 +18,17 @@ promo.addEventListener( 'mouseout', function(e){
     promo.style.transform = "scale(70%)"
     } );
 
+
+let shower = document.getElementById("Shower");
+let No1 = document.getElementById("No1");
+let pageArticle01 = document.getElementById("codeArticle01")
+
+shower.addEventListener("click", function(e){
+    promo.style.display = "none";
+    No1.style.display = "none";
+    pageArticle01.style.display = "flex";
+    articleSet.style.flexDirection = "column"
+    article.style.flexWrap = "wrap"
+    
+
+})
